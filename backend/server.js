c9fd1ab2602fe7d0637ae31ba771a063b0846a5c
Faeiz <faeizhamdrad48@gmail.com>
2026-01-13 17:13:12 +0430

    dockerized app 1

--- backend/server.js
+++ backend/server.js
@@ -18,8 +18,9 @@ const app = express();
 // Security middleware
 app.use(helmet());
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-    credentials: true
+    origin: [ process.env.FRONTEND_URL || 'http://localhost:5173',
+    'http://192.168.56.10:5173' ],
+    credentials: true 
 }));
 app.use(cookieParser());
 app.use(express.json());
