77bc87018fbac7f7083f894cd59b075d16008a4a
Faeiz <faeizhamdrad48@gmail.com>
2026-01-13 23:49:45 +0430

    edit server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,8 @@ const app = express();
 // Security middleware
 app.use(helmet());
 app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    origin: [ process.env.FRONTEND_URL || 'http://localhost:5173',
+    'http://localhost:5173' ],
     credentials: true 
 }));
 app.use(cookieParser());
